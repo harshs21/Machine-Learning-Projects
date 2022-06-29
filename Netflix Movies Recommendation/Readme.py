5e48c2da3095bb1e35826962f4e68aaa82377066
harshs21 <55891603+harshs21@users.noreply.github.com>
2022-06-29 20:41:26 +0530

    Create Readme.py

---
+++ Netflix Movies Recommendation/Readme.py
@@ -0,0 +1,5 @@
+If the github .ipynb file does not work then go to this links given bellow
+1.	Netfix Movies Recomendations
+https://colab.research.google.com/drive/1KnYgDfjFvMuilcVgVxvSqna0m4aCFfqc?usp=sharing 
+2.	Hybrid Recommendation system for Good read books dataset
+https://colab.research.google.com/drive/1oKRCiPjScQz5TEWqXwi9ZAxCebIb3mBb?usp=sharing 
