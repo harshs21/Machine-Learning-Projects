18b2eff58f7f077509740ed9aba8a2154f461c80
harshs21 <55891603+harshs21@users.noreply.github.com>
2022-08-10 02:22:43 +0530

    Create Readme.py

---
+++ Quora Question Pairs/Readme.py
@@ -0,0 +1,8 @@
+ATTENTION!!!!
+__If any of the above given github .ipynb file does not work then go to these links given bellow__
+
+Quora Question Similarity Detection 
+•	Cosine Similiarity
+https://colab.research.google.com/drive/1FwWV7O-bG6vA4zVgaSink6LktPRJi7Fd?usp=sharing 
+•	Using tf-idf vectorizer and doc2vec 
+https://colab.research.google.com/drive/1CY2m6nobWHYhlZ0G49kRy0zB93LPrtQ0?usp=sharing 
